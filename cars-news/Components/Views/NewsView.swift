538f4663410811cd872bf11ad0546826f481bfd3
IKIS <polyanaozero@gmail.com>
2022-09-04 13:36:06 +0300

    feature/news-page: news page layout setted

--- cars-news/Components/Views/NewsView.swift
+++ cars-news/Components/Views/NewsView.swift
@@ -13,24 +13,30 @@ final class NewsView: UIView {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = 12
+        imageView.layer.cornerRadius = 22
+        imageView.clipsToBounds = true
         imageView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         
         return imageView
     }()
     
-    private let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return scrollView
-    }()
-    
     private let newsTextStack: UIStackView = {
         let stack = UIStackView()
-        stack.distribution = .fillEqually
+        stack.distribution = .equalSpacing
         stack.axis = .vertical
         stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 8
+        stack.alignment = .leading
+        
+        return stack
+    }()
+    
+    private let newsBodyStack: UIStackView = {
+        let stack = UIStackView()
+        stack.distribution = .equalSpacing
+        stack.axis = .vertical
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 8
         
         return stack
     }()
@@ -47,29 +53,32 @@ final class NewsView: UIView {
     
     private let newsText: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.textColor = Colors.secondary.color
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = Colors.headerBackground.color
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         
         return label
     }()
     
-    private let newsSrc: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.textColor = Colors.secondary.color
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 0
-        label.text = "Подробнее..."
+    private let srcNewsButton: UIButton = {
+        let button = UIButton(type: .system)
         
-        return label
+        button.layer.cornerRadius = 12
+        button.backgroundColor = Colors.headerBackground.color
+        button.clipsToBounds = true
+        button.setTitle("Подробнее...", for: .normal)
+        button.tintColor = Colors.secondary.color
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.titleEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        return button
     }()
     
     private let categoryType: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textColor = Colors.secondary.color
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .systemGray5
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         
@@ -78,68 +87,55 @@ final class NewsView: UIView {
     
     private let publishedDateNews: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textColor = Colors.secondary.color
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = Colors.light.color
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         
         return label
     }()
     
-    private let header: Header = Header()
-    
     public func initialize(image: UIImage?, title: String, body: String, date: String, fullUrl: String, category: String) {
         self.imageView.image = image
         self.newsTitle.text = title
         self.newsText.text = body
         self.categoryType.text = category
-        self.publishedDateNews.text = date
+        self.publishedDateNews.text = "Дата размещения: \(date.formatToDate())"
         
         self.setupStyle()
     }
     
+    // MARK: private methods
+    /// Setup layout for view
     private func setupStyle() {
         self.addSubview(self.imageView)
-        self.addSubview(self.scrollView)
-        
-        self.scrollView.addSubview(self.newsTextStack)
+        self.imageView.addSubview(self.newsTextStack)
+        self.addSubview(self.newsBodyStack)
         
         self.newsTextStack.addArrangedSubview(self.newsTitle)
         self.newsTextStack.addArrangedSubview(self.categoryType)
-        self.newsTextStack.addArrangedSubview(self.newsText)
-        self.newsTextStack.addArrangedSubview(self.newsSrc)
-        self.newsTextStack.addArrangedSubview(self.publishedDateNews)
+        self.newsTextStack.addArrangedSubview(self.srcNewsButton)
         
-        self.newsTextStack.setCustomSpacing(400, after: self.newsSrc)
+        self.newsBodyStack.addArrangedSubview(self.newsText)
+        self.newsBodyStack.addArrangedSubview(self.publishedDateNews)
         
         self.imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        self.imageView.heightAnchor.constraint(lessThanOrEqualToConstant: 200).isActive = true
         self.imageView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        self.imageView.heightAnchor.constraint(lessThanOrEqualToConstant: UIScreen.main.bounds.height * 0.6).isActive = true
         
-        self.scrollView.topAnchor.constraint(equalTo: self.imageView.bottomAnchor).isActive = true
-        self.scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        self.scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        self.scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        self.newsTextStack.leadingAnchor.constraint(equalTo: self.imageView.leadingAnchor, constant: 6).isActive = true
+        self.newsTextStack.trailingAnchor.constraint(equalTo: self.imageView.trailingAnchor, constant: -6).isActive = true
+        self.newsTextStack.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -12).isActive = true
+        self.newsTextStack.widthAnchor.constraint(equalTo: self.imageView.widthAnchor).isActive = true
         
-        self.newsTextStack.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor).isActive = true
-        self.newsTextStack.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor).isActive = true
-        self.newsTextStack.topAnchor.constraint(equalTo: self.scrollView.topAnchor).isActive = true
-        self.newsTextStack.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor).isActive = true
-        
-        self.newsTextStack.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor).isActive = true
-        
-        self.newsText.leadingAnchor.constraint(equalTo: self.newsTextStack.leadingAnchor, constant: 12).isActive = true
-        
-        self.newsTitle.leadingAnchor.constraint(equalTo: self.newsTextStack.leadingAnchor, constant: 12).isActive = true
-        
-        self.scrollView.delegate = self
+        self.newsBodyStack.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 6).isActive = true
+        self.newsBodyStack.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -6).isActive = true
+        self.newsBodyStack.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 12).isActive = true
         
         self.backgroundColor = Colors.background.color
     }
-}
-
-extension NewsView: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("_LOG_ did sroll")
+    
+    @objc func openNewsSrc() {
+        print("_LOG_ was open")
     }
 }
--- cars-news/Utils/Colors.swift
+++ cars-news/Utils/Colors.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 enum Colors {
-    case newsItemMain, background, newsCellShadow, headerBackground, secondary
+    case newsItemMain, background, newsCellShadow, headerBackground, secondary, light
     
     var color: UIColor {
         switch self {
@@ -24,6 +24,9 @@ enum Colors {
             
         case .background:
             return UIColor(red: 255/255, green: 231/255, blue: 191/255, alpha: 1)
+        
+        case .light:
+            return UIColor(red: 1, green: 140/255, blue: 140/255, alpha: 1)
             
         case .newsCellShadow:
             return UIColor(red: 161/255, green: 0, blue: 53/255, alpha: 0.8)
